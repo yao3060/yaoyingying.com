0bd6bb4f2d2851108c6cea6c8323c8da07156b1e
YAOYINGYING <yao3060@gmail.com>
2022-03-15 23:28:10 +0800

    add yaoin_get_post_thumbnail

---
+++ src/wp-content/themes/onepress-child/template-parts/content.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Template part for displaying posts.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package OnePress
+ */
+
+$show_thumbnail = true;
+if (get_theme_mod('onepress_hide_thumnail_if_not_exists', false)) {
+    if (!has_post_thumbnail()) {
+        $show_thumbnail = false;
+    }
+}
+
+?>
+<article id="post-<?php the_ID(); ?>" <?php post_class(array('list-article', 'clearfix')); ?>>
+    <?php if ($show_thumbnail) { ?>
+        <div class="list-article-thumb">
+            <a href="<?php echo esc_url(get_permalink()); ?>">
+                <?php echo yaoin_get_post_thumbnail(); ?>
+            </a>
+        </div>
+    <?php } ?>
+
+    <div class="list-article-content">
+        <div class="list-article-meta">
+            <?php the_category(' / '); ?>
+        </div>
+        <header class="entry-header">
+            <?php the_title(sprintf('<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url(get_permalink())), '</a></h2>'); ?>
+        </header><!-- .entry-header -->
+        <div class="entry-excerpt">
+            <?php
+            the_excerpt();
+            ?>
+            <?php
+            wp_link_pages(
+                array(
+                    'before' => '<div class="page-links">' . esc_html__('Pages:', 'onepress'),
+                    'after'  => '</div>',
+                )
+            );
+            ?>
+        </div><!-- .entry-content -->
+    </div>
+
+</article><!-- #post-## -->
