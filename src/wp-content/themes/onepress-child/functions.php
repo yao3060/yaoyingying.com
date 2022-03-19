918570e37166671e5900dbfb10bca9cbc5e7c430
YAOYINGYING <yao3060@gmail.com>
2022-03-19 22:42:11 +0800

    ..

--- src/wp-content/themes/onepress-child/functions.php
+++ src/wp-content/themes/onepress-child/functions.php
@@ -1,1 +1,10 @@
 <?php
+
+add_action('wp_enqueue_scripts', function () {
+    wp_enqueue_style(
+        'onepress-child-style',
+        get_stylesheet_uri(),
+        array('onepress-style'),
+        wp_get_theme()->get('Version') // this only works if you have Version in the style header
+    );
+});
